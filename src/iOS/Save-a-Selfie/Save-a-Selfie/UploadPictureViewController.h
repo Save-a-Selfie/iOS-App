2ee6a0498e02a5fd776d22a94884bfdbda3bc3b4
Nadja Deininger <nadja@ef.gy>
2014-05-12 23:44:04 +0100

    iOS: changes to UI for picture upload

---
+++ src/iOS/Save-a-Selfie/Save-a-Selfie/UploadPictureViewController.h
@@ -0,0 +1,13 @@
+//
+//  UploadPictureViewController.h
+//  Save-a-Selfie
+//
+//  Created by Nadja Deininger on 12/05/14.
+//  Copyright (c) 2014 Code for All Ireland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UploadPictureViewController : UIViewController
+
+@end
---
+++ src/iOS/Save-a-Selfie/Save-a-Selfie/UploadPictureViewController.m
@@ -0,0 +1,49 @@
+//
+//  UploadPictureViewController.m
+//  Save-a-Selfie
+//
+//  Created by Nadja Deininger on 12/05/14.
+//  Copyright (c) 2014 Code for All Ireland. All rights reserved.
+//
+
+#import "UploadPictureViewController.h"
+
+@interface UploadPictureViewController ()
+
+@end
+
+@implementation UploadPictureViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
