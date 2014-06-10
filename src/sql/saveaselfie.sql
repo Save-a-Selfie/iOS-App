ac8baddae74823976d4dd342f0384922c885ca81
Nadja Deininger <nadja@ef.gy>
2014-06-10 18:46:06 +0100

    store GPS coordinates in decimal to avoid conversions

--- src/sql/saveaselfie.sql
+++ src/sql/saveaselfie.sql
@@ -3,22 +3,13 @@ create table users
 	name text not null primary key
 );
 
-create type latref as enum ('E', 'W');
-create type longref as enum ('N', 'S');
-
 create table photos
 (
 	id integer not null primary key,
 
 	-- GPS data
-	latituderef latref not null,
-	latdegrees integer not null,
-	latminutes integer not null,
-	latseconds numeric not null,
-	longituderef longref not null,
-	longdegrees integer not null,
-	longminutes integer not null,
-	longseconds numeric not null,
+	latitude numeric not null,
+	longitude numeric not null,
 
 	image bytea not null,
 	thumbnail bytea not null,
