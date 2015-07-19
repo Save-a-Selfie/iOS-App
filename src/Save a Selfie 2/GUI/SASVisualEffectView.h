cf651d1d6ebbfb656f90f3340f74c972b7ba0ab2
Stephen Fox <stephenfox995@hotmail.com>
2015-07-19 16:27:07 +0100

    Added visual effect class.

---
+++ src/Save a Selfie 2/GUI/SASVisualEffectView.h
@@ -0,0 +1,15 @@
+//
+//  SASVisualEffectView.h
+//  Save a Selfie
+//
+//  Created by Stephen Fox on 18/07/2015.
+//  Copyright (c) 2015 Stephen Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SASVisualEffectView : UIVisualEffectView
+
+
+
+@end
---
+++ src/Save a Selfie 2/GUI/SASVisualEffectView.m
@@ -0,0 +1,17 @@
+//
+//  SASVisualEffectView.m
+//  Save a Selfie
+//
+//  Created by Stephen Fox on 18/07/2015.
+//  Copyright (c) 2015 Stephen Fox. All rights reserved.
+//
+
+#import "SASVisualEffectView.h"
+
+@implementation SASVisualEffectView
+
+
+
+
+
+@end
