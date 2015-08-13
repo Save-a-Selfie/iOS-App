19e23f76e777079ba42177f711a32205d2b250cb
Stephen Fox <stephenfox995@hotmail.com>
2015-08-14 00:46:54 +0100

    Finished page control on SASInforamtionViewController

--- src/Save a Selfie 2/GUI/SASInformationViewController/SASInformationViewController.m
+++ src/Save a Selfie 2/GUI/SASInformationViewController/SASInformationViewController.m
@@ -13,7 +13,9 @@
 #import "Screen.h"
 #import "SASSponsorCardManager.h"
 
-@interface SASInformationViewController ()
+
+
+@interface SASInformationViewController () <UIScrollViewDelegate>
 
 @property (strong, nonatomic) UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
@@ -34,6 +36,7 @@
     self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, [Screen width], [Screen height])];
     self.scrollView.contentSize = CGSizeMake([Screen width] * [SASSponsorCardManager sponsorAmount], 300);
     self.scrollView.pagingEnabled = YES;
+    self.scrollView.delegate = self;
     [self.view addSubview:self.scrollView];
 }
 
@@ -62,5 +65,39 @@
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.facebook.com/pages/Adams-Gift/334232113442348"]];
 }
 
+
+
+
+#pragma mark <UIScrollViewDelegate>
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    CGFloat xOffset = scrollView.contentOffset.x;
+    self.pageControl.currentPage = [self determinePageIndex:xOffset];
+}
+
+#pragma Helper methods.
+// Determines the page index from the scrollview offset.
+// Pages start at 0 all the way up to n...
+- (NSUInteger) determinePageIndex:(CGFloat) offset {
+    
+// TODO: This could be improved instead of checking for each page separately.
+// However, there will most likely not be a large ammount of sponsors, so this could do.
+    
+    if (offset == [Screen width] * 0) {
+        return 0;
+    } else if (offset == [Screen width] * 1) {
+        return 1;
+    } else if (offset == [Screen width] * 2) {
+        return 2;
+    } else if (offset == [Screen width] * 3) {
+        return 3;
+    } else if (offset == [Screen width] * 4) {
+        return 4;
+    } else if (offset == [Screen width] * 5) {
+        return 5;
+    } else {
+        return NSIntegerMax;
+    }
+
+}
 
 @end
